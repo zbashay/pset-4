4b7c1374e570157e6830bbb52b09fa3475fd1025
Zaviay Bashay <ronatorino@gmail.com>
2019-11-21 10:26:17 -0500

    prime

---
+++ src/reverse.js
@@ -0,0 +1,26 @@
+const readlineSync = require("readline-sync");
+
+const MIN = Number.MIN_SAFE_INTEGER;
+const MAX = Number.MAX_SAFE_INTEGER;
+
+let integer = 0;
+let print = "";
+
+console.log();
+
+while (integer <= 0 || integer > MAX || !Number.isInteger(integer)) {
+  integer = Number(readlineSync.question("Positive integer: "));
+}
+
+while (integer > 0) {
+  let a = integer % 10;
+  integer = Math.floor(integer / 10);
+
+  if (integer > 0) {
+  print = print + a + ", ";
+} else {
+  print = print + a + "."
+}
+}
+
+  console.log("\n" + print);

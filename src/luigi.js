c4aae6defb56d3965dc5dffecc0122ab2b1fc24a
zbashay <zbashay@ucvts.org>
2019-11-24 21:28:59 -0500

    Do you know how long it took to make this? TOO LONG

--- src/luigi.js
+++ src/luigi.js
@@ -2,6 +2,9 @@ const readlineSync = require("readline-sync");
 
 var theHeight = 0;
 var space = "##";
+var opposite = "##"
+var theHeight2 = theHeight;
+var theHeight3 = theHeight;
 
 console.log("");
 
@@ -9,19 +12,30 @@ do {
       theHeight = Number(readlineSync.question("Height: "));
 } while (theHeight < 1 || theHeight > 24 || Number.isNaN(theHeight) || !Number.isInteger(theHeight));
 
-let theHeight2 = theHeight;
+var theHeight2 = theHeight;
+var theHeight3 = theHeight;
 
 while (theHeight2 > 1) {
       space = " " + space
-    --theHeight2
+      --theHeight2
 }
 
-console.log("\n" + space)
+while (theHeight3 > 1) {
+      opposite = opposite + " "
+      --theHeight3
+}
+
+console.log("\n" + space + "  " + opposite)
+
 
 while (theHeight > 1) {
     var space1 = space.replace(" #", "##")
-    console.log(space1)
+    var opposite1 = opposite.replace("# ", "##")
+
+    console.log(space1 + "  " + opposite1)
     --theHeight
+
     var space = space1
+    var opposite = opposite1
 }
-console.log()
+console.log("")
